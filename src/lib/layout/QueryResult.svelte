f87ca1b3f71a319d146ea9922af392693d558318
JuleZ <julestblt.dev@pm.me>
2023-08-04 16:01:01 +0200

    feat: add copy button and update variant

--- src/lib/layout/QueryResult.svelte
+++ src/lib/layout/QueryResult.svelte
@@ -1,24 +1,35 @@
 <script lang="ts">
 	import Textfield from '@smui/textfield';
 	import Button, { Label, Icon } from '@smui/button';
+	import IconButton from '@smui/icon-button';
 
 	export let result: string;
 
+	const buildUrl = (query: string) => {
+		return `https://www.google.ru/search?q=${query}`;
+	};
+
 	const openDork = () => {
-		window.open(`https://www.google.ru/search?q=${result}`, '_blank');
+		window.open(buildUrl(result), '_blank');
+	};
+
+	const sendToClipboard = () => {
+		navigator.clipboard.writeText(buildUrl(result));
 	};
 </script>
 
 <div class="result">
 	<h2>Constructed Result :</h2>
-	<Textfield
-		input$readonly
-		class="shaped-filled"
-		variant="filled"
-		style="width: 100%;"
-		helperLine$style="width: 100%;"
-		bind:value={result}
-	/>
+	<span>
+		<Textfield
+			input$readonly
+			variant="outlined"
+			style="width: 100%;"
+			helperLine$style="width: 100%;"
+			bind:value={result}
+		/>
+		<IconButton on:click={sendToClipboard} class="material-icons">content_paste</IconButton>
+	</span>
 	<Button variant="raised" style="width: 100%; margin-top: 1rem" on:click={openDork}
 		><Label>Open This Dork</Label><Icon class="material-icons">link</Icon></Button
 	>
@@ -27,5 +38,10 @@
 <style>
 	.result {
 		width: 48%;
+	}
+
+	span {
+		display: flex;
+		align-items: center;
 	}
 </style>
